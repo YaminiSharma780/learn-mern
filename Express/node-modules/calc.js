a379d273cadf7eadc67b9e5bd35f688cccffdef2
Yamini <sharmayamini1999@gmail.com>
2023-07-08 18:21:49 +0530

    first commit

---
+++ Express/node-modules/calc.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return (a+b);
+}
+function subtract(a,b){
+    return (a-b);
+}
+function multiply(a,b){
+    return (a*b);
+}
+function modulus(a,b){
+    return (a%b);
+}
+module.exports.add = add;
+module.exports.subtract=subtract;
+module.exports.multiply=multiply;
+module.exports.modulus=modulus;
